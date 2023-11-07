fcae1e51e329a7f8acb2c499a9ec2db2bbd8fdc8
FernandoJocev <fernandojocevine@gmail.com>
2023-11-07 10:17:08 +0700

    Home section

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 const Navbar = () => {
   return (
     <>
-      <div className='absolute top-0 flex justify-between min-w-full z-50 pl-20 pr-20 pt-10 items-center'>
+      <div className='absolute top-0 flex justify-between min-w-full z-50 pl-40 pr-40 pt-10 items-center'>
         <h4 className='text-[#FAF9F9] font-bold'>Epje</h4>
         <div className='flex gap-x-20 items-center'>
           <div className='text-[#2B2218] flex font-bold gap-x-20 items-center'>
--- app/page.tsx
+++ app/page.tsx
@@ -6,23 +6,48 @@ const page = () => {
   return (
     <>
       <Navbar />
-      <div id='bg' className='relative'>
-        <img
-          src='/images/bg-main.png'
-          alt='Background'
-          className='min-w-full'
-        />
+      <section id='home' className='relative'>
+        <span
+          id='circle'
+          className='top-[-148px] left-[-155px] w-[310px] h-[297px]'
+        ></span>
+        <span
+          id='circle-nofill'
+          className='top-[-204px] left-[-193px] w-[424px] h-[409px]'
+        ></span>
+        <span
+          id='circle-nofill'
+          className='top-[-190px] right-[-170px] w-[424px] h-[409px]'
+        ></span>
         <img
           src='/images/Me-nobg.png'
           alt='Me'
           className='absolute bottom-0 min-w-full'
         />
-      </div>
-      <section id='home' className='absolute z-50 top-[40%] left-[17%]'>
-        <h1 className='text-center text-[#2B2218]'>
-          Hi! I’m Fernando Jocevine ! <br /> Also known as Epje !
+        <h1 className='text-center text-[#2B2218] absolute top-[53%] left-[15%] font-semibold text-[20px]'>
+          Hi! I’m <b>Fernando Jocevine ! </b> <br /> Also known as<b> Epje !</b>
         </h1>
       </section>
+      <span
+        id='circle'
+        className='bottom-[-72px] left-[200px] w-[135px] h-[123px] !bg-[#E2B587] opacity-[80%]'
+      ></span>
+      <div className='relative top-[190px]'>
+        <span
+          id='circle'
+          className='bottom-[142px] right-[100px] w-[135px] h-[123px] !bg-[#E2B587] opacity-[80%] -z-10'
+        ></span>
+        <span
+          id='circle-nofill'
+          className='bottom-[132px] right-[180px] w-[135px] h-[123px] -z-10'
+        ></span>
+      </div>
+      <h1 className='text-center mt-[5%] font-semibold text-[24px]'>
+        I’m very interested in
+        <br /> <b className='text-[#EDC191]'> Web Dev </b> &
+        <b className='text-[#EDC191]'> Mobile Dev. </b>
+      </h1>
+      <section id='about' className='relative'></section>
     </>
   )
 }
